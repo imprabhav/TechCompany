1f6b894be9950a4bc6ac53edd0d351cefca7af84
Prabhav Srivastava <68961083+imprabhav@users.noreply.github.com>
2024-10-08 21:37:18 +0530

    Update Navbar.jsx

--- src/Components/Home/Navbar/Navbar.jsx
+++ src/Components/Home/Navbar/Navbar.jsx
@@ -146,7 +146,7 @@ const Navbar = () => {
               Home
             </Link>
             <Link
-              to="/about"
+              to="/about-us"
               className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
             >
               About Us

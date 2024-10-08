af5a4ae823661e3ab591e5910d2534ea4b14860d
Prabhav Srivastava <68961083+imprabhav@users.noreply.github.com>
2024-10-08 21:26:10 +0530

    Update MobileDev2.jsx

--- src/Components/Services/MobileDev2.jsx
+++ src/Components/Services/MobileDev2.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import MobileDevelopmentImage from '../../assets/Mobile.jpg'; 
+import MobileDevelopmentImage from '../../assets/mobile.jpg'; 
 
 const MobileDevelopmentPage = () => {
     return (
